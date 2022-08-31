d3cafad830e046e2da3070a14b23ad929801f2e7
cortadocodes <cortado.codes@protonmail.com>
2022-08-31 15:07:07 +0100

    FEA: Move queries over from aerosense-dashboard

---
+++ aerosense_tools/queries.py
@@ -0,0 +1,222 @@
+import datetime as dt
+from google.cloud import bigquery
+
+
+ROW_LIMIT = 10000
+
+
+# SCRATCH: MIGHT BE USEFUL, OTHERWISE GET RID:
+# ============================================
+# AND datetime > DATE_ADD(PARSE_DATETIME('%Y%m%d', @DS_START_DATE), INTERVAL @hour HOUR)
+# AND datetime < DATE_ADD(PARSE_DATETIME('%Y%m%d', @DS_START_DATE), INTERVAL @hour+1 HOUR)
+# AND IS_NAN(sensor_value[ORDINAL(1)]) IS FALSE
+# AND IS_NAN(sensor_value[ORDINAL(2)]) IS FALSE
+# AND IS_NAN(sensor_value[ORDINAL(3)]) IS FALSE
+# AND IS_NAN(sensor_value[ORDINAL(4)]) IS FALSE
+
+
+class BigQuery:
+    def __init__(self):
+        self.client = bigquery.Client()
+
+    def get_sensor_data(
+        self,
+        installation_reference,
+        node_id,
+        sensor_type_reference,
+        start=None,
+        finish=None,
+    ):
+        """Get sensor data for the given sensor type on the given node of the given installation over the given time
+        period. The time period defaults to the last day.
+
+        :param str installation_reference:
+        :param str|None node_id:
+        :param str sensor_type_reference:
+        :param datetime.datetime|None start:
+        :param datetime.datetime|None finish:
+        :param bool all_time:
+        :return pandas.Dataframe:
+        """
+        table_name = f"aerosense-twined.greta.sensor_data_{sensor_type_reference}"
+
+        conditions = """
+        WHERE datetime BETWEEN @start AND @finish
+        AND installation_reference = @installation_reference
+        AND node_id = @node_id
+        """
+
+        start, finish = self._get_time_period(start, finish)
+
+        query_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("start", "DATETIME", start),
+                bigquery.ScalarQueryParameter("finish", "DATETIME", finish),
+                bigquery.ScalarQueryParameter("installation_reference", "STRING", installation_reference),
+                bigquery.ScalarQueryParameter("node_id", "STRING", node_id),
+            ]
+        )
+
+        count_query = f"""
+        SELECT COUNT(datetime)
+        FROM `{table_name}`
+        {conditions}
+        """
+
+        number_of_rows = list(self.client.query(count_query, job_config=query_config).result())[0][0]
+
+        data_query = f"""
+        SELECT *
+        FROM `{table_name}`
+        {conditions}
+        ORDER BY datetime
+        """
+
+        data_limit_applied = False
+
+        if number_of_rows > ROW_LIMIT:
+            data_query += f"\nLIMIT {ROW_LIMIT}"
+            data_limit_applied = True
+
+        return (self.client.query(data_query, job_config=query_config).to_dataframe(), data_limit_applied)
+
+    def get_sensor_data_at_datetime(
+        self,
+        installation_reference,
+        node_id,
+        sensor_type_reference,
+        datetime,
+        tolerance=1,
+    ):
+        """Get sensor data for the given sensor type on the given node of the given installation at the given datetime.
+        The first datetime within a tolerance of plus/minus half of `tolerance` is used.
+
+        :param str installation_reference:
+        :param str|None node_id:
+        :param str sensor_type_reference:
+        :param datetime.datetime|None datetime:
+        :param float tolerance: the tolerance on the given datetime in seconds
+        :return pandas.Dataframe:
+        """
+        table_name = f"aerosense-twined.greta.sensor_data_{sensor_type_reference}"
+
+        conditions = """
+        WHERE datetime >= @start_datetime
+        AND datetime < @finish_datetime
+        AND installation_reference = @installation_reference
+        AND node_id = @node_id
+        """
+
+        start_datetime = datetime - dt.timedelta(seconds=tolerance / 2)
+        finish_datetime = datetime + dt.timedelta(seconds=tolerance / 2)
+
+        query_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("start_datetime", "DATETIME", start_datetime),
+                bigquery.ScalarQueryParameter("finish_datetime", "DATETIME", finish_datetime),
+                bigquery.ScalarQueryParameter("installation_reference", "STRING", installation_reference),
+                bigquery.ScalarQueryParameter("node_id", "STRING", node_id),
+            ]
+        )
+
+        query = f"""
+        SELECT *
+        FROM `{table_name}`
+        {conditions}
+        ORDER BY datetime
+        """
+
+        return self.client.query(query, job_config=query_config).to_dataframe()
+
+    def get_aggregated_connection_statistics(
+        self,
+        installation_reference,
+        node_id,
+        start=None,
+        finish=None,
+    ):
+        """Query for minute-wise aggregated connection statistics over a day, by default the day up to now.
+
+        :param [str] installation_reference: The installation reference to query for, e.g. "ost-wt-tests"
+        :param Union[datetime.datetime, None] up_to: The point in time, default now(), where results will
+        be delivered for the 24 hours prior.
+        """
+        connection_statistics_agg_sql = """
+        SELECT datetime, filtered_rssi, raw_rssi, tx_power, allocated_heap_memory
+        FROM `aerosense-twined.greta.connection_statistics_agg`
+        WHERE datetime BETWEEN @start AND @finish
+        AND installation_reference = @installation_reference
+        AND node_id = @node_id
+        ORDER BY datetime
+        """
+
+        start, finish = self._get_time_period(start, finish)
+
+        query_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("start", "DATETIME", start),
+                bigquery.ScalarQueryParameter("finish", "DATETIME", finish),
+                bigquery.ScalarQueryParameter("installation_reference", "STRING", installation_reference),
+                bigquery.ScalarQueryParameter("node_id", "STRING", node_id),
+            ]
+        )
+
+        return self.client.query(connection_statistics_agg_sql, job_config=query_config).to_dataframe()
+
+    def get_installations(self):
+        """Query for all installations (without sensor coordinate data)"""
+        installations_sql = """
+        SELECT reference, turbine_id, location
+        FROM `aerosense-twined.greta.installation`
+        ORDER BY reference
+        """
+
+        installations = self.client.query(installations_sql).to_dataframe().to_dict(orient="records")
+
+        return [
+            {"label": f"{row['reference']} (Turbine {row['turbine_id']})", "value": row["reference"]}
+            for row in installations
+        ]
+
+    def get_sensor_types(self):
+        """Get the available sensor types.
+
+        :return list:
+        """
+        query = """
+        SELECT name
+        FROM `aerosense-twined.greta.sensor_type`
+        ORDER BY name
+        """
+
+        return self.client.query(query).to_dataframe()["name"].to_list()
+
+    def get_nodes(self, installation_reference):
+        """Get the IDs of the nodes available for the given installation.
+
+        :param str installation_reference:
+        :return list(str):
+        """
+        query = """
+        SELECT node_id FROM `aerosense-twined.greta.sensor_data`
+        WHERE installation_reference = @installation_reference
+        GROUP BY node_id
+        ORDER BY node_id
+        """
+
+        query_config = bigquery.QueryJobConfig(
+            query_parameters=[bigquery.ScalarQueryParameter("installation_reference", "STRING", installation_reference)]
+        )
+
+        return self.client.query(query, job_config=query_config).to_dataframe()["node_id"].to_list()
+
+    def _get_time_period(self, start=None, finish=None):
+        """Get the time period for the query. Defaults to the past day.
+
+        :param datetime.datetime|None start:
+        :param datetime.datetime|None finish:
+        :return (datetime.datetime, datetime.datetime):
+        """
+        finish = finish or dt.datetime.now()
+        start = start or finish - dt.timedelta(days=1)
+        return start, finish
