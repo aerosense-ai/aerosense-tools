7fb7e17a55afc42857e3e8a17566654b4402c6b8
Juri <yuriy.marykovskiy@mail.polimi.it>
2022-12-08 10:44:22 +0100

    STY: Fix code style

--- aerosense_tools/preprocess.py
+++ aerosense_tools/preprocess.py
@@ -1,15 +1,16 @@
+import datetime as dt
 import logging
 import numpy as np
 import pandas as pd
-import datetime as dt
 from scipy.interpolate import interp1d
+
 from aerosense_tools.plots import plot_with_layout
+
 
 logger = logging.getLogger(__name__)
 
 
 class RawSignal:
-
     def __init__(self, dataframe, sensor_type):
         self.dataframe = dataframe
         self.sensor_type = sensor_type
@@ -40,7 +41,7 @@ class RawSignal:
         self.dataframe = self.dataframe[
             (self.dataframe <= rolling_median + standard_deviation_multiplier * rolling_std)
             & (self.dataframe >= rolling_median - standard_deviation_multiplier * rolling_std)
-            ]
+        ]
 
     def measurement_to_variable(self, sensor_conversion_constants=None):
         """Transform fixed point values to a physical variable.
@@ -59,10 +60,10 @@ class RawSignal:
         default_conversion_constants = {
             "barometer": 40.96,  # [Pa]
             "barometer_thermometer": 100,  # [Celsius]
-            "differential_barometer": (58982 - 6553)/differential_pressure_range,  # [Pa]
-            "accelerometer": accelerometer_sensitivity/gravitational_acceleration,  # [m/s²]
+            "differential_barometer": (58982 - 6553) / differential_pressure_range,  # [Pa]
+            "accelerometer": accelerometer_sensitivity / gravitational_acceleration,  # [m/s²]
             "gyroscope": gyroscope_sensitivity * 180 / np.pi,  # [s⁻¹]
-            "magnetometer": 1  # TODO this is tbd.
+            "magnetometer": 1,  # TODO this is tbd.
         }
         sensor_conversion_constants = sensor_conversion_constants or default_conversion_constants
 
@@ -81,43 +82,40 @@ class RawSignal:
         measurement_sessions = []
         sample_time = pd.DataFrame(self.dataframe.index)
 
-        session_starts = sample_time['datetime'].diff() > threshold
-        session_ends = abs(sample_time['datetime'].diff(-1)) > threshold
+        session_starts = sample_time["datetime"].diff() > threshold
+        session_ends = abs(sample_time["datetime"].diff(-1)) > threshold
         session_starts.iloc[0] = session_ends.iloc[-1] = True
 
         # Edge case of a single measurement point:
         single_sample = sample_time.index[session_starts] == sample_time.index[session_ends]
         if any(single_sample):
             logger.warning(
-                'Sensor type {} has single measurement points at {}'.format(
-                    self.sensor_type,
-                    sample_time[session_starts][single_sample]
+                "Sensor type {} has single measurement points at {}".format(
+                    self.sensor_type, sample_time[session_starts][single_sample]
                 )
             )
 
         start_rows = sample_time.index[session_starts][~single_sample]
         end_rows = sample_time.index[session_ends][~single_sample]
 
-        session_times = pd.DataFrame({
-            "start": sample_time["datetime"][start_rows].to_list(),
-            "end": sample_time["datetime"][end_rows].to_list()
-        })
+        session_times = pd.DataFrame(
+            {"start": sample_time["datetime"][start_rows].to_list(), "end": sample_time["datetime"][end_rows].to_list()}
+        )
 
         for start_row, end_row in zip(start_rows, end_rows):
             measurement_sessions.append(
-                SensorMeasurementSession(
-                    self.dataframe.iloc[start_row:end_row, :],
-                    self.sensor_type
-                )
+                SensorMeasurementSession(self.dataframe.iloc[start_row:end_row, :], self.sensor_type)
             )
 
         return measurement_sessions, session_times
 
 
 class SensorMeasurementSession:
-    """A class representing a continuous measurement series for a particular sensor.
+    """A class representing continuous measurement series for a particular sensor. The class wraps some frequently used
+    Pandas.DataFrame operations as well as plotly figure setup.
 
     """
+
     def __init__(self, dataframe, sensor_type):
         self.dataframe = dataframe
         self.sensor_type = sensor_type
@@ -134,11 +132,7 @@ class SensorMeasurementSession:
         """
 
         old_time_vector = self.dataframe.index.values.astype(np.int64)
-        new_time_vector = pd.date_range(
-            start=timeseries_start or self.start,
-            end=self.end,
-            freq=time_step
-        )
+        new_time_vector = pd.date_range(start=timeseries_start or self.start, end=self.end, freq=time_step)
 
         new_dataframe = pd.DataFrame(index=new_time_vector)
 
@@ -155,9 +149,8 @@ class SensorMeasurementSession:
         :param datetime.timedelta trim_from_end: Amount of time to trim from the end of the session
         :return SensorMeasurementSession: sensor session with trimmed_dataframe
         """
-        time_window = (
-                (self.dataframe.index > self.start + trim_from_start) &
-                (self.dataframe.index < self.end - trim_from_end)
+        time_window = (self.dataframe.index > self.start + trim_from_start) & (
+            self.dataframe.index < self.end - trim_from_end
         )
 
         trimmed_dataframe = self.dataframe[time_window]
@@ -174,16 +167,16 @@ class SensorMeasurementSession:
         """
 
         plot_max_time = plot_max_time or self.duration - plot_start_offset
-        plot_df = self.trim_session(plot_start_offset, self.duration - plot_start_offset-plot_max_time).dataframe
+        plot_df = self.trim_session(plot_start_offset, self.duration - plot_start_offset - plot_max_time).dataframe
 
         if sensor_names:
             plot_df = plot_df[sensor_names]
 
         layout = {
-            "title": sensor_types_metadata[self.sensor_type]['description'],
+            "title": sensor_types_metadata[self.sensor_type]["description"],
             "xaxis_title": "Date/Time",
-            "yaxis_title": sensor_types_metadata[self.sensor_type]['variable'],
-            "legend_title": "Sensor"
+            "yaxis_title": sensor_types_metadata[self.sensor_type]["variable"],
+            "legend_title": "Sensor",
         }
         figure = plot_with_layout(plot_df, layout_dict=layout)
         return figure
