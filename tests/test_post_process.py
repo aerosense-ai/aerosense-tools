d735929a0d2883cf13a8d339ddc7066b846fd9f3
Juri <yuriy.marykovskiy@mail.polimi.it>
2023-03-14 15:01:45 +0100

    TST:  Add post-process IMU test

--- tests/test_post_process.py
+++ tests/test_post_process.py
@@ -1,3 +1,4 @@
+import logging
 import unittest
 import datetime as dt
 import numpy as np
@@ -8,6 +9,7 @@ from aerosense_tools.postprocess import BladeIMU, PostProcess
 
 from tests import TEST_DURATION, TEST_START_TIME, TEST_SAMPLING_STEP
 
+logger = logging.getLogger(__name__)
 
 class TestPostProcess(unittest.TestCase):
 
@@ -26,20 +28,20 @@ class TestPostProcess(unittest.TestCase):
         sample_dataframe = pd.DataFrame(index=time_vector)
         sample_dataframe.index.name="datetime"
         # Make 10 rotations
-        sample_dataframe['test_azimuth'] = np.linspace(0, 10*2*np.pi, len(time_vector)) % (2*np.pi)
+        sample_dataframe['test_azimuth'] = np.linspace(0, 10*2*np.pi, len(time_vector)) % (2*np.pi) * (-1)
         return sample_dataframe
 
     def sample_accelerometer(self):
-        base_data = self.sample_timeseries(TEST_START_TIME, 10*TEST_DURATION, TEST_SAMPLING_STEP)
-        base_data["Acc_Dir_1"]=base_data["test_azimuth"].apply(np.sin) * 9.8
-        base_data["Acc_Dir_2"] = base_data["test_azimuth"].apply(np.cos) * (- 9.8) + 10*9.8
+        base_data = self.sample_timeseries(TEST_START_TIME, 10*TEST_DURATION, TEST_SAMPLING_STEP/10)
+        base_data["Acc_Dir_1"]=base_data["test_azimuth"].apply(np.sin) * (-9.8)
+        base_data["Acc_Dir_2"] = base_data["test_azimuth"].apply(np.cos) * (-9.8) - 10*9.8
         base_data["Acc_Dir_3"] = 0
         base_data.drop("test_azimuth", axis=1, inplace=True)
         accelerometer = SensorMeasurementSession(base_data, "accelerometer")
         return accelerometer
 
     def sample_gyrometer(self):
-        base_data = self.sample_timeseries(TEST_START_TIME, 10*TEST_DURATION, TEST_SAMPLING_STEP)
+        base_data = self.sample_timeseries(TEST_START_TIME, 10*TEST_DURATION, TEST_SAMPLING_STEP/10)
         base_data["Gyro_Dir_1"] = 0
         base_data["Gyro_Dir_2"] = 0
         base_data["Gyro_Dir_3"] = - 2*np.pi
@@ -64,3 +66,11 @@ class TestPostProcess(unittest.TestCase):
         self.assertIsInstance(imu, BladeIMU)
 
 
+    def test_postprocess_imu(self):
+        accelerometer = self.sample_accelerometer()
+        gyrometer = self.sample_gyrometer()
+        wind_turbine_metadata = {"hub_height": 30}
+        imu_reference_frame = {"angles":[0 ,0, 0], "r-coordinate":10}
+        processed_data = PostProcess.process_imu(accelerometer, gyrometer, imu_reference_frame, wind_turbine_metadata)
+
+        self.assertTrue(processed_data["height"].iloc[0], 40)
